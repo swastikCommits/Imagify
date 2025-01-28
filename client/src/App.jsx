4555ab32b6b8add3b46c72d5ec37486dd0449c67
swastikCommits <textswastik.alt@gmail.com>
2025-01-28 06:25:48 +0530

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-teal-100'>
+    <div className='px-8 sm:px-12 md:px-16 lg:px-36 min-h-screen bg-gradient-to-b from-teal-50 to-teal-100'>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/result" element={<Result />} /> 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+    const [user, setUser] = useState(true); 
+    const navigate = useNavigate();
+
+  return (
+    <div className='flex justify-between items-center py-4'>
+        <Link to='/'>
+        <img src={assets.logo} alt=""  className='w-28 sm:w-32 lg:w-40'/>
+        </Link>
+
+        <div>
+            {user ? 
+            <div className='flex items-center gap-2 sm:gap-3'>
+                <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full gover:scale-105 transition-all duration-700'>
+                    <img className='w-5' src={assets.credit_star} alt=""/>
+                    <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left : 50</p>
+                </button>
+                <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Swastik</p>
+                <div className='relative group'>
+                    <img src={assets.profile_icon} alt="" className='w-10 drop-shadow'/>
+                    <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                        <ul>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                </div>
+            </div> 
+            : 
+            <div className='flex items-center gap-2 sm:gap-5'>
+                <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                <button className="bg-zinc-800 text-white px-7 py-2 sm:pz-10 text-sm rounded-full">Login</button>
+            </div>}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
