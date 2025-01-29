fcff39b13e38156c9c1fac02d45abec6bc24cbeb
swastikCommits <textswastik.alt@gmail.com>
2025-01-29 16:46:16 +0530

    init

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 
 const Result = () => {
   return (
+    <form>
     <div>
-      Result Page
+      <div className="relative">    
+        <img src={assets.sample_img_1} alt="" className="max-w-sm rounded"/>
+        <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"/>
+      </div>
+      <p>Loading...</p>
+    
     </div>
+    <div>
+      <input type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20"/>
+      <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">Generate</button>
+    </div>
+    </form>
   );
 };
 
