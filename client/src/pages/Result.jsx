3f7444c08f5f22fa8b13395445ff465bc8f76369
swastikCommits <textswastik.alt@gmail.com>
2025-01-31 21:32:08 +0530

    init

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -7,13 +7,18 @@ const Result = () => {
   const [image, setImage]=useState(assets.sample_img_1);
   const [isImageLoaded, setIsImageLoaded]=useState(false);
   const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('');
+
+  const onSubmitHandler = async (e) => {
+    
+  }
 
   return (
-    <form className="flex flex-col items-center justify-center min-h-[90vh]">
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center justify-center min-h-[90vh]">
     <div>
       <div className="relative">    
         <img src={image} alt="" className="max-w-sm rounded"/>
-        <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"/>
+        <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`}/>
       </div>
       <p className={!loading ? 'hidden' : ''}>Loading...</p>
     
@@ -21,7 +26,8 @@ const Result = () => {
 
   {!isImageLoaded && 
     <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
-      <input type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20c placeholder-color"/>
+      
+      <input onChange={e=>setInput(e.target.value)} value={input} type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20c placeholder-color"/>
       <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">Generate</button>
     </div>
   }
