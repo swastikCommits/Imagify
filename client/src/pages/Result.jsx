12ad0e6e089cc202cf943a74a8e98f3c9cc92bc1
swastikCommits <textswastik.alt@gmail.com>
2025-01-31 06:05:46 +0530

    init

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 
 const Result = () => {
+
+  const [image, setImage]=useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded]=useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
     <form className="flex flex-col items-center justify-center min-h-[90vh]">
     <div>
       <div className="relative">    
-        <img src={assets.sample_img_1} alt="" className="max-w-sm rounded"/>
+        <img src={image} alt="" className="max-w-sm rounded"/>
         <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"/>
       </div>
-      <p>Loading...</p>
+      <p className={!loading ? 'hidden' : ''}>Loading...</p>
     
     </div>
+
+  {!isImageLoaded && 
     <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
-      <input type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20"/>
+      <input type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20c placeholder-color"/>
       <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">Generate</button>
     </div>
+  }
+
+  {isImageLoaded &&
+    <div className="flex gap-2 flex-wrap justify-cenetr text-white text-sm p-0.5 mt-10 rounded-full">
+      <p onClick={()=>setIsImageLoaded(false)} className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">Generate Another</p>
+      <a href={image} download className="bg-zinc-900 px-10 py-3 cursor-pointer rounded-full">Download</a>
+    </div>
+ }  
     </form>
   );
 };
