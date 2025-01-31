ad8283fc7611c72872b235d6453a0f97882e6baf
swastikCommits <textswastik.alt@gmail.com>
2025-01-31 21:44:14 +0530

    init

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { plans } from "../assets/assets";
+import { assets } from "../assets/assets";
 
 
 const BuyCredit = () => {
   return (
-    <div>
-      Buy Credit
+    <div className="min-h-[80vh] text-center pt-14 mb-10 ">
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">Our Plans</button>
+      <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">Choose the plan</h1>
+
+      <div>
+        {plans.map((item, index)=>(
+          <div key={index}>
+            <img src={assets.lock_icon} alt=""/>
+            <p>{item.id}</p>
+            <p>{item.desc}</p>
+            <p>{item.price} / {item.credits} credits</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
