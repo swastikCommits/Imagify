c360083485732678de40ca32d049271f7db92d58
swastikCommits <textswastik.alt@gmail.com>
2025-02-01 09:52:50 +0530

    init

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -9,13 +9,16 @@ const BuyCredit = () => {
       <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">Our Plans</button>
       <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">Choose the plan</h1>
 
-      <div>
+      <div className="flex flex-wrap justify-center gap-6 text-left">
         {plans.map((item, index)=>(
-          <div key={index}>
-            <img src={assets.lock_icon} alt=""/>
-            <p>{item.id}</p>
-            <p>{item.desc}</p>
-            <p>{item.price} / {item.credits} credits</p>
+          <div key={index} className="bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500">
+            <img width={40} src={assets.logo_icon} alt=""/>
+            <p className="mt-3 mb-1 font-semibold">{item.id}</p>
+            <p className="text-sm">{item.desc}</p>
+            <p className="mt-6">
+              <span className="text-3xl font-medium">${item.price} </span>${item.price} / {item.credits} credits
+            </p>
+            <button className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52">Get Started</button>
           </div>
         ))}
       </div>
