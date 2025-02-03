6b30c4759d802dc2520b90a25768c829d60bf7cf
swastikCommits <textswastik.alt@gmail.com>
2025-02-03 20:27:58 +0530

    init

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,13 +5,13 @@ const Login = () => {
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       
-        <form>
-            <h1>Sign Up</h1>
-            <p>Welcome back! Please sign in to continue</p>
+        <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+            <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+            <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
-            <div>
+            <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                 <img src={assets.user_icon} alt=""/>
-                <input type="text" placeholder='Full name' required/>
+                <input type="text" className='outline-none tetx-sm' placeholder='Full Name' required/>
             </div>
         </form>
 
