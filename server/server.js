cdccd5f72b01f8eacc6b2dd3d069964595e40da4
swastikCommits <textswastik.alt@gmail.com>
2025-02-07 20:38:45 +0530

    init

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv/config');
+const connectDB = require('./config/mongodb.js');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -8,10 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+(async () => {
+    try{
+        await connectDB(); 
+    } catch (error) {
+        console.log('Error connecting to the database');
+    }
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
