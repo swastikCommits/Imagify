154814f7ba86b05c64fbfa205844b8114d3ca94b
swastikCommits <textswastik.alt@gmail.com>
2025-02-11 20:43:09 +0530

    init

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,9 +6,14 @@ export const AppContext = createContext();
 const AppContextProvider = (props)=>{
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const [credit, setCredit] = useState(false);
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const value={
-        user, setUser, showLogin, setShowLogin
+        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit
     }       
     return (
         <AppContext.Provider value={value}>
