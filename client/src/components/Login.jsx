c0309a8fecb0f579c522239340275d7d63c92eb0
swastikCommits <textswastik.alt@gmail.com>
2025-02-12 16:29:50 +0530

    init

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,13 +2,38 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { motion } from 'framer-motion'
-
+import axios from 'axios'
 
 const Login = () => {
 
     const [state, setState] = useState('Login');
-    const {setShowLogin} = useContext(AppContext);
- 
+    const {setShowLogin, backendUrl, setToken, setUser} = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault();
+
+        try{
+            if(state === 'Login'){
+                const data = axios.post(backendUrl +  '/api/user/login', {email, password});
+                
+                if(data.success){
+                    setToken(data.token);
+                    setUser(data.user);
+                    localStorage.setItem('token', data.token);
+                    setShowLogin(false);
+                }
+                else{
+
+                }
+
+        } catch (error) {
+
+        }
+    }
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
@@ -20,7 +45,7 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       
-        <motion.form 
+        <motion.form onSubmit={onSubmitHandler}
         initial={{opacity:0.2, y: 50}}
         transition={{duration: 0.3}}
         whileInView={{opacity:1, y:0}}
@@ -31,17 +56,17 @@ const Login = () => {
 
             { state !=='Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.user_icon} alt=""/>
-                <input type="text" className='outline-none tetx-sm' placeholder='Full Name' required/>
+                <input onChange={e=>setName(e.target.value)} value={name} type="text" className='outline-none tetx-sm' placeholder='Full Name' required/>
             </div>}
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                 <img src={assets.email_icon} alt=""/>
-                <input type="email" className='outline-none tetx-sm' placeholder='Email id' required/>
+                <input onChange={e=>setEmail(e.target.value)} value={email} type="email" className='outline-none tetx-sm' placeholder='Email id' required/>
             </div>
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt=""/>
-                <input type="password" className='outline-none tetx-sm' placeholder='Password' required/>
+                <input onChange={e=>setPassword(e.target.value)} value={password} type="password" className='outline-none tetx-sm' placeholder='Password' required/>
             </div>
 
             <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
