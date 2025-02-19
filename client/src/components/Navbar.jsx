bf39a281c10635aad51d18054cccf778c486471b
swastikCommits <textswastik.alt@gmail.com>
2025-02-19 07:48:43 +0530

    init

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             </div> 
             : 
             <div className='flex items-center gap-2 sm:gap-5'>
-                <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                {/* <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p> */}
                 <button onClick={()=>setShowLogin(true)} className="bg-zinc-800 text-white px-7 py-2 sm:pz-10 text-sm rounded-full cursor-pointer">Login</button>
             </div>}
         </div>
