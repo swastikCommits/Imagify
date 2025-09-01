c4641f2c449d2d5c2d4071f735de1f5f1b18a762
Swastik Ranjan Panigrahi <textswastik.alt@gmail.com>
2025-09-01 16:51:32 +0530

    Update Result.jsx

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -51,7 +51,7 @@ const Result = () => {
     <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
       
       <input onChange={e=>setInput(e.target.value)} value={input} type="text" placeholder="Describe what you want to generate " className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20c placeholder-color"/>
-      <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">Generate</button>
+      <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full cursor-pointer">Generate</button>
     </div>
   }
 
@@ -64,5 +64,6 @@ const Result = () => {
     </motion.form>
   );
 };
+
 
 export default Result;
